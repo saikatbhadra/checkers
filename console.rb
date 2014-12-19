315c00f9009b315ef8c9ab0a87d1bee6c264af36
Saikat Bhadra <saikat.bhadra@gmail.com>
2014-12-18 23:34:38 -0800

    Add file to capture arrow keys

---
+++ console.rb
@@ -0,0 +1,18 @@
+module Console
+  require 'io/console'
+  def self.read_char
+    STDIN.echo = false
+    STDIN.raw!
+
+    input = STDIN.getc.chr
+    if input == "\e"
+      input << STDIN.read_nonblock(3) rescue nil
+      input << STDIN.read_nonblock(2) rescue nil
+    end
+  ensure
+    STDIN.echo = true
+    STDIN.cooked!
+
+    return input
+  end
+end
